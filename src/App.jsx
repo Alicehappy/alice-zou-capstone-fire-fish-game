d3bd47139ce5ade51a2f558f85f707a1f4ba1784
Alice Zou <Alicehappy@users.noreply.github.com>
2024-10-21 09:34:29 -0400

    Setup global style and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TypingGamePage from "./pages/TypingGamePage";
-import SubmitScorePage from "./pages/SubmitScorePage";
-import ScoreboardPage from "./pages/ScoreboardPage";
-import AnimalSelectionPage from "./pages/AnimalSelectionPage";
-import Page404 from "./pages/AnimalSelectionPage";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import TypingGamePage from "./pages/TypingGamePage/TypingGamePage";
+import SubmitScorePage from "./pages/SubmitScorePage/SubmitScorePage";
+import ScoreboardPage from "./pages/ScoreboardPage/ScoreboardPage";
+import AnimalSelectionPage from "./pages/AnimalSelectionPage/AnimalSelectionPage";
+import Page404 from "./pages/Page404/Page404";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
 import "./HomePage.scss";
 
 function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="home__title">Welcome to home page</h1>
+      <p className="home__content">This is the home content.</p>
+    </div>
+  );
 }
 
 export default HomePage;
